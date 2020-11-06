61f336b32e29bbf2cab68453c0091c5828cc9412
Rhamer Noro <rhamernoro@gmail.com>
2020-11-05 21:47:30 -0400

    tela inicial + cadastro de livro

---
+++ app/src/main/java/com/example/controle_livros/cadastro_livro.java
@@ -0,0 +1,14 @@
+package com.example.controle_livros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cadastro_livro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_livro);
+    }
+}
